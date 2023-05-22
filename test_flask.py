f9e79503a18533897d03100cd5762d2373d08e78
aalexeei <kai2001@ukr.net>
2023-05-22 11:39:21 +0300

    add work with requirements

--- test_flask.py
+++ test_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,Response
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/requirements/')
+def requirements():
+    with open('requirements.txt', 'r') as file:
+        content = file.read()
+        content_with_newline = content + '\n'
+        return Response(content_with_newline, mimetype='text/plain')
+
+
+
+
 
 
 if __name__ == '__main__':
